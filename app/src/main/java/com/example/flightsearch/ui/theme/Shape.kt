931fcacbaf0c19ed383abcfc4d91fe28b44e295b
PaoloCaldera <paolo.caldera@outlook.com>
2025-08-28 12:25:48 +0200

    feature: add shape resources in the application theme
    Define the shape of the ui components for each size in the Shape.kt file.
    
    Integrates #1

---
+++ app/src/main/java/com/example/flightsearch/ui/theme/Shape.kt
@@ -0,0 +1,13 @@
+package com.example.flightsearch.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    extraSmall = RoundedCornerShape(25.dp),
+    small = RoundedCornerShape(25.dp),
+    medium = RoundedCornerShape(25.dp),
+    large = RoundedCornerShape(25.dp),
+    extraLarge = RoundedCornerShape(25.dp)
+)
