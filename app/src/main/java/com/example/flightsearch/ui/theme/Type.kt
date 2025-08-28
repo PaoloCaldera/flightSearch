1f4043898643e1c89b66defa84b3f17c08897d72
PaoloCaldera <paolo.caldera@outlook.com>
2025-08-28 12:27:47 +0200

    feature: add typography resources in the application theme
    Use the google provider in order to retrieve the Ubuntu font to use in the application. Add the corresponding dependency in the build.gradle.kts and libs.versions.toml files.
    Update all the versions of the libraries to get the latest ones.
    Create the dimens.xml file for future dimension references.
    
    Integrates #1

--- app/src/main/java/com/example/flightsearch/ui/theme/Type.kt
+++ app/src/main/java/com/example/flightsearch/ui/theme/Type.kt
@@ -1,34 +1,76 @@
 package com.example.flightsearch.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import com.example.flightsearch.R
 
-// Set of Material typography styles to start with
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+val displayFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Ubuntu"),
+        weight = FontWeight.Bold,
+        style = FontStyle.Normal,
+        fontProvider = provider
+    )
+)
+val headlineFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Ubuntu"),
+        weight = FontWeight.Bold,
+        style = FontStyle.Normal,
+        fontProvider = provider
+    )
+)
+val titleFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Ubuntu"),
+        weight = FontWeight.Medium,
+        style = FontStyle.Normal,
+        fontProvider = provider
+    )
+)
+val bodyFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Ubuntu"),
+        weight = FontWeight.Normal,
+        style = FontStyle.Normal,
+        fontProvider = provider
+    )
+)
+val labelFontFamily = FontFamily(
+    Font(
+        googleFont = GoogleFont("Ubuntu"),
+        weight = FontWeight.Light,
+        style = FontStyle.Normal,
+        fontProvider = provider
+    )
+)
+
+// Default Material 3 typography values
+val baseline = Typography()
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    displayLarge = baseline.displayLarge.copy(fontFamily = displayFontFamily),
+    displayMedium = baseline.displayMedium.copy(fontFamily = displayFontFamily),
+    displaySmall = baseline.displaySmall.copy(fontFamily = displayFontFamily),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = headlineFontFamily),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = headlineFontFamily),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = headlineFontFamily),
+    titleLarge = baseline.titleLarge.copy(fontFamily = titleFontFamily),
+    titleMedium = baseline.titleMedium.copy(fontFamily = titleFontFamily),
+    titleSmall = baseline.titleSmall.copy(fontFamily = titleFontFamily),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = bodyFontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = bodyFontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = bodyFontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = labelFontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = labelFontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = labelFontFamily),
 )
